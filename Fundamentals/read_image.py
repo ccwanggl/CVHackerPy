1a7f4710d5b8bf3a3519492b5a4b7a88998f1f5e
guoliang wang <tswanggl@hotmail.com>
2021-02-16 21:38:23 +0800

    remove rescale code into subdir

--- Fundamentals/read_image.py
+++ Fundamentals/read_image.py
@@ -1,18 +1,11 @@
 import cv2.cv2 as cv
 
-
-def rescaleFrame(frame, scale=0.75):
-    width = int(frame.shape[1] * scale)
-    height = int(frame.shape[0] * scale)
-
-    dimensions = (width, height)
-    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
-
+from code_snip import rescale
 
 img = cv.imread('../assets/earth.jpg', -1)
 img_rotate = cv.rotate(img, cv.ROTATE_90_COUNTERCLOCKWISE)
 cv.imshow('Image', img)
-cv.imshow('Resized Image', rescaleFrame(img))
+cv.imshow('Resized Image', rescale.rescaleFrame(img))
 cv.imshow('Rotated Image', img_rotate)
 cv.waitKey(0)
 cv.destroyAllWindows()
