abec553f6ac8e12d5ef74e567d18260e22822947
guoliang wang <tswanggl@hotmail.com>
2021-02-16 21:32:06 +0800

    rescale frame in function

--- Fundamentals/read_image.py
+++ Fundamentals/read_image.py
@@ -1,10 +1,18 @@
 import cv2.cv2 as cv
 
+
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+
+    dimensions = (width, height)
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+
 img = cv.imread('../assets/earth.jpg', -1)
-img_size = cv.resize(img,(0,0), fx=1/2, fy=1/2)
 img_rotate = cv.rotate(img, cv.ROTATE_90_COUNTERCLOCKWISE)
 cv.imshow('Image', img)
-cv.imshow('Resized Image', img_size)
+cv.imshow('Resized Image', rescaleFrame(img))
 cv.imshow('Rotated Image', img_rotate)
-cv.waitKey(10)
+cv.waitKey(0)
 cv.destroyAllWindows()
