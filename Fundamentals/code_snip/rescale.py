e7d20b97e4a00bab618a14fba75e483d328f6fa0
guoliang wang <tswanggl@hotmail.com>
2021-02-17 09:53:22 +0800

    use catpure object to set the width and height

--- Fundamentals/code_snip/rescale.py
+++ Fundamentals/code_snip/rescale.py
@@ -2,6 +2,7 @@ import cv2.cv2 as cv
 
 
 def rescaleFrame(frame, scale=0.75):
+    # image, live video
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
 
--- Fundamentals/videocapture.py
+++ Fundamentals/videocapture.py
@@ -1,14 +1,16 @@
 import cv2.cv2 as cv
 from code_snip import rescale
+from code_snip import resize_live_video
 
 capture = cv.VideoCapture(0)
+resize_live_video.changeRes(100, 200, capture)
 
 while True:
     isTrue, frame = capture.read()
     cv.imshow('Video', frame)
-    cv.imshow('Recaled Video', rescale.rescaleFrame(frame, .2))
+    # cv.imshow('Recaled Video', rescale.rescaleFrame(frame, .2))
 
-    if cv.waitKey(0) & 0xFF == ord('d'):
+    if cv.waitKey(20) & 0xFF == ord('d'):
         break
 
 capture.release()
