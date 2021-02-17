1d17412cfb094a37dc513930661b7523cdcbb0d0
guoliang wang <tswanggl@hotmail.com>
2021-02-17 16:59:00 +0800

    compare matplotlib and cv::show

--- Fundamentals/color_space.py
+++ Fundamentals/color_space.py
@@ -1,15 +1,24 @@
 import cv2.cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 
 img = cv.imread('../Resources/Photos/cats.jpg')
 cv.imshow('Cats', img)
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
+plt.imshow(img)
+plt.show()
 
 
-hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-cv.imshow('HSV', hsv)
+#gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#cv.imshow('Gray', gray)
+#
+#
+#hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+#cv.imshow('HSV', hsv)
+#
+#lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+#cv.imshow('LAB', lab)
+
 
 cv.waitKey(0)
 cv.destroyAllWindows()
