a3b1672eb9b1a23b0a537fbcf2981abc9cb5cdae
guoliang wang <tswanggl@hotmail.com>
2021-02-17 12:47:09 +0800

    Convert image to gray

--- Fundamentals/contours.py
+++ Fundamentals/contours.py
@@ -1,8 +1,11 @@
 import cv2.cv2 as cv
 
 
-img = cv.imread('../Resources/Photos/cat.jpg')
+img = cv.imread('../Resources/Photos/cats.jpg')
 cv.imshow('Cats', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
 
 
 cv.waitKey()
