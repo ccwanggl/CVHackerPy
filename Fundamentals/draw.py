70131fbd797a671001e7655dd2c27c273190bfb1
guoliang wang <tswanggl@hotmail.com>
2021-02-17 10:12:19 +0800

    put text on image

--- Fundamentals/draw.py
+++ Fundamentals/draw.py
@@ -8,20 +8,21 @@ cv.imshow('Blank', blank)
 blank[:] = 0, 0, 255
 cv.imshow('Red', blank)
 
-
 # 2 Draw
-cv.rectangle(blank,(0,0), (300, 400), (0,255,0), thickness = 2)
+cv.rectangle(blank, (0, 0), (300, 400), (0, 255, 0), thickness=2)
 cv.imshow('Rect', blank)
 
-
-cv.rectangle(blank,(0,0), (300, 400), (255,255,0), thickness=-1)
+cv.rectangle(blank, (0, 0), (300, 400), (255, 255, 0), thickness=-1)
 cv.imshow('Brush', blank)
 
-cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (255,0,0), thickness=-1)
+cv.circle(blank, (blank.shape[1] // 2, blank.shape[0] // 2), 40, (255, 0, 0), thickness=-1)
 cv.imshow('Circle', blank)
 
-cv.line(blank, (0, 0), (blank.shape[1]//2, blank.shape[0]//2), (255,255,255), thickness=4)
+cv.line(blank, (0, 0), (blank.shape[1] // 2, blank.shape[0] // 2), (255, 255, 255), thickness=4)
 cv.imshow('Line', blank)
+
+cv.putText(blank, "Guoliang", (225, 225), cv.FONT_HERSHEY_COMPLEX, 1.0, (255, 233, 122), thickness=2)
+cv.imshow("text", blank)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
