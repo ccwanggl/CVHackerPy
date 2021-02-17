80c520e6e261cb3ff0bcf656431a68ca8318e657
guoliang wang <tswanggl@hotmail.com>
2021-02-17 09:45:50 +0800

    Add rescale to video capture

--- Fundamentals/videocapture.py
+++ Fundamentals/videocapture.py
@@ -1,12 +1,14 @@
 import cv2.cv2 as cv
+from code_snip import rescale
 
 capture = cv.VideoCapture(0)
 
 while True:
     isTrue, frame = capture.read()
     cv.imshow('Video', frame)
+    cv.imshow('Recaled Video', rescale.rescaleFrame(frame, .2))
 
-    if cv.waitKey(20) & 0xFF == ord('d'):
+    if cv.waitKey(0) & 0xFF == ord('d'):
         break
 
 capture.release()
