5f66a2f5be3adee93c5526d32c744a6701690026
guoliang wang <tswanggl@hotmail.com>
2021-02-17 12:09:00 +0800

    erode

--- Fundamentals/essential_functions.py
+++ Fundamentals/essential_functions.py
@@ -3,14 +3,13 @@ import cv2.cv2 as cv
 img = cv.imread('../Resources/Photos/cat.jpg')
 cv.imshow('Cat', img)
 
-
 # Converting to grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow("Gray", gray)
 
 # Blur
 
-blur = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT)
+blur = cv.GaussianBlur(img, (3, 3), cv.BORDER_DEFAULT)
 cv.imshow("Blur", blur)
 
 # Edge Cascade
@@ -18,8 +17,14 @@ canny = cv.Canny(img, 125, 175)
 cv.imshow('Canny', canny)
 
 # Dilating the image
-dilated = cv.dilate(canny, (7,7), iterations= 3)
+dilated = cv.dilate(canny, (7, 7), iterations=3)
 cv.imshow('Dilated', dilated)
+
+# Eroding
+eroded = cv.erode(dilated, (7, 7), iterations=3)
+cv.imshow('Eroded', eroded)
+
+
 
 cv.waitKey(0)
 cv.destroyAllWindows()
