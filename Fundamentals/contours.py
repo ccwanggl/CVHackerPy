1c9697bffa086fcecdfcb219f7e8b818e10dbb04
guoliang wang <tswanggl@hotmail.com>
2021-02-17 12:55:22 +0800

    update

--- Fundamentals/contours.py
+++ Fundamentals/contours.py
@@ -7,6 +7,15 @@ cv.imshow('Cats', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
+blur = cv.GaussianBlur(gray, (5, 5), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
+
+
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny', canny)
+
+contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+print(f'{len(contours)} contour(s) found!')
 
 cv.waitKey()
 cv.destroyAllWindows()
