6546f94e7a6c5d65de454850c300d65dccb6ce90
guoliang wang <tswanggl@hotmail.com>
2021-02-17 10:07:16 +0800

    draw some basic shape

--- Fundamentals/draw.py
+++ Fundamentals/draw.py
@@ -1,4 +1,25 @@
 import cv2.cv2 as cv
 import numpy as np
 
+blank = np.zeros((500, 500, 3), dtype='uint8')
+cv.imshow('Blank', blank)
 
+# Paint the image a certain color
+blank[:] = 0, 0, 255
+cv.imshow('Red', blank)
+
+
+# 2 Draw
+cv.rectangle(blank,(0,0), (300, 400), (0,255,0), thickness = 2)
+cv.imshow('Rect', blank)
+
+
+cv.rectangle(blank,(0,0), (300, 400), (255,255,0), thickness=-1)
+cv.imshow('Brush', blank)
+
+cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (255,0,0), thickness=-1)
+cv.imshow('Circle', blank)
+
+
+cv.waitKey(0)
+cv.destroyAllWindows()
