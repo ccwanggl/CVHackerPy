c5349a151f9a03bfd8909778529d3dc8f37b1b49
guoliang wang <tswanggl@hotmail.com>
2021-02-17 13:04:48 +0800

    how to draw contours

--- Fundamentals/contours.py
+++ Fundamentals/contours.py
@@ -1,21 +1,30 @@
 import cv2.cv2 as cv
+import numpy as np
 
 
 img = cv.imread('../Resources/Photos/cats.jpg')
 cv.imshow('Cats', img)
 
+blank = np.zeros(img.shape, dtype='uint8')
+
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
-blur = cv.GaussianBlur(gray, (5, 5), cv.BORDER_DEFAULT)
-cv.imshow('Blur', blur)
-
-
-canny = cv.Canny(blur, 125, 175)
+#blur = cv.GaussianBlur(gray, (5, 5), cv.BORDER_DEFAULT)
+#cv.imshow('Blur', blur)
+#
+#
+canny = cv.Canny(gray, 125, 175)
 cv.imshow('Canny', canny)
 
-contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
+cv.imshow('Thresh', thresh)
+
+contours, hierarchies = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
 print(f'{len(contours)} contour(s) found!')
+
+cv.drawContours(blank, contours, -1, (0, 0, 255), 1)
+cv.imshow('Contour', blank)
 
 cv.waitKey()
 cv.destroyAllWindows()
