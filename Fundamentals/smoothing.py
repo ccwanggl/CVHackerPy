7d2e02471beb112a16b889f3716cd87b6881eeed
guoliang wang <tswanggl@hotmail.com>
2021-02-19 19:33:22 +0800

    average blur

--- Fundamentals/smoothing.py
+++ Fundamentals/smoothing.py
@@ -3,6 +3,9 @@ import cv2.cv2 as cv
 img = cv.imread('../Resources/Photos/cats.jpg')
 cv.imshow('Cats', img)
 
+# averaging blur
+average = cv.blur(img, (3, 3))
+cv.imshow('Average', average)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
