1c865be846f339366fc6a8c290275f43575c2c69
guoliang wang <tswanggl@hotmail.com>
2021-02-19 19:23:21 +0800

    Merged

--- Fundamentals/splitmerge.py
+++ Fundamentals/splitmerge.py
@@ -4,7 +4,18 @@ import numpy as np
 img = cv.imread('../Resources/Photos/Cats.jpg')
 cv.imshow('Cats', img)
 
+blank = np.zeros(img.shape[:2], "uint8")
+
 b, g, r = cv.split(img)
+
+blue = cv.merge([b, blank, blank])
+green = cv.merge([blank, g, blank])
+red = cv.merge([blank, blank, r])
+
+cv.imshow('BlueMerged', blue)
+cv.imshow('GreenMerged', green)
+cv.imshow('RedMerged', red)
+
 cv.imshow('Blue', b)
 cv.imshow('Green', g)
 cv.imshow('Red', r)
